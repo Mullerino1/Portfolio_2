4d903a0e987fc0d44710bbe0be0cae780a27a6c1
Mullerino1 <martinemu03@gmail.com>
2024-08-29 20:09:01 +0200

    Testing
    lots of testing and failing -w-

--- portfolio_2/main.js
+++ portfolio_2/main.js
@@ -1,8 +1,39 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
+let DatabaseInfo = 
+`[
+    {
+       "Name": "Martine",
+       "Title": "Title",
+       "Date": "Date",
+       "Link": "here's a link",
+       "Description": "Description",
+       "projects": ["Portfolio_1", "Portfolio_2"]
+           }
+    ]
+`
+   
+       console.log(JSON.parse(DatabaseInfo))
+
+
+
+       const formApplication = document.querySelector('.form')
+
+       formApplication.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const formData = new FormData(formApplication)
+        const data = Object.fromEntries(formData)
+
+        fetch('http://localhost:5173/Home', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+       })
 
 
